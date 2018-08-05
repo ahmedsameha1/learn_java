ac080f450063528961e8bf5054fb63c3eb27e0c6
Ahmed Mohamed Sameh <ahmedsameha1@gmail.com>
2018-08-05 07:13:43 +0200

    first try with OptionalInt

--- README.md
+++ README.md
@@ -3,3 +3,5 @@ https://www.apress.com/gp/book/9781484228432
 https://www.apress.com/gp/book/9781484233474#otherversion=9781484233481
 I'm trying to implement what I learn using complete programs. Each program has its own directory in the repository.
 I use Gradle as the build tool.
+
+.gitignore file content is copied from: https://raw.githubusercontent.com/github/gitignore/master/Gradle.gitignore
---
+++ optional/src/main/java/com/ahmedsameha1/learn_java/optional/LearnOptional.java
@@ -0,0 +1,28 @@
+package com.ahmedsameha1.learn_java.optional;
+
+import java.util.OptionalInt; // Copied from: https://docs.oracle.com/javase/8/docs/api/java/util/OptionalInt.html
+import java.util.Arrays; // Copied from: https://docs.oracle.com/javase/7/docs/api/java/util/Arrays.html
+
+public class LearnOptional {
+    public static void main(String[] args) {
+        var obj = new LearnOptional();
+        var optionalResult = obj.sumOver100(1, 3, 6, 111);
+        if ( optionalResult.isPresent()) {
+            System.out.println("The result is: " + optionalResult.getAsInt());
+        } else {
+            System.out.println("The result is below 100");
+        }
+    }
+
+    // Returns OptionalInt that has the result if the result is over
+    // 100, otherwise returns empty OptionalInt
+    public OptionalInt sumOver100(int... ints) {
+        var intStream = Arrays.stream(ints);
+        var sum = intStream.reduce(0, Integer::sum);
+        if (sum > 100) {
+            return OptionalInt.of(sum);
+        } else {
+            return OptionalInt.empty();
+        }
+    }
+}
